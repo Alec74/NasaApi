d97f46532dbf2c70e992540c358f7706c0079a2c
Alec74 <alecmcglone@gmail.com>
2022-01-19 11:16:46 -0500

    components stlyed using bulma, like btn added, header added

--- client/src/components/NasaImg.js
+++ client/src/components/NasaImg.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 const NasaImg = ({ nasa }) => {
 
+  const [like, setLike] = useState("is-primary is-outlined button fas fa-star")
 
+  const handleClick = (e) => {
+    if (like === "is-primary is-outlined button far fa-star") {
+      setLike("is-primary is-outlined button fas fa-star")
+      e.target.className = like
+    } else {
+      setLike("is-primary is-outlined button far fa-star")
+      e.target.className = like
+    }
+
+
+  }
 
   // console.log(nasa);
   let date = new Date(nasa.data[0].date_created).toLocaleDateString(
@@ -16,30 +28,37 @@ const NasaImg = ({ nasa }) => {
   const pic = nasa.links[0].href
 
   return (
-    <div className="column is-12-mobile is-3-desktop is-4-tablet is-one-fifth-fullhd">
+    <div className="column is-12-mobile is-4-desktop is-4-tablet is-one-fifth-fullhd">
       <div className="nasa" className="card">
         {/* <h2 >{nasa.Title}</h2> */}
-        <header className="card-header">
-          <p className="card-header-title">
-            {nasa.data[0].title}
-          </p>
-          <p>{date}</p>
-        </header>
+        <div className="card-header">
+          <h3 className="card-header-title">
+            <p >
+              {nasa.data[0].title}
+            </p>
+            {/* <p>{date}</p> */}
+          </h3>
+          <h4 className="card-header-title">
+            {date}
+          </h4>
+        </div>
         <div className="card-image click">
           {/* <form className="js-modal-trigger" onClick={handleToggle} data-target="modal"> */}
-          <figure className="image is-2by3">
-            <img
-              width="200"
-              alt={`The image titled: ${nasa.data[0].title}`}
-              src={pic}
-            />
+          <figure className="image is-2by2">
+            <a href={pic}>
+              <img
+                width="200"
+                alt={`The image titled: ${nasa.data[0].title}`}
+                src={pic}
+              />
+            </a>
           </figure>
           {/* </form> */}
         </div>
-        <footer className="card-footer">
+        <div className="text content has-text-justified">
           {description != null ? (
             <>
-              <p dangerouslySetInnerHTML={{ __html: description }}>
+              <p className="desc" dangerouslySetInnerHTML={{ __html: description }}>
               </p>
               <br></br>
             </>
@@ -48,7 +67,8 @@ const NasaImg = ({ nasa }) => {
               <br>
               </br>
             )}
-        </footer>
+        </div>
+        <button className="is-primary is-outlined button far fa-star" onClick={handleClick}></button>
 
       </div>
 
